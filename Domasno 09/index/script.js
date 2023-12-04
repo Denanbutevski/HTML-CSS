e2293ee6d0d608ce27a5ec528f95174cb69d27fd
Denanbutevski <149868378+Denanbutevski@users.noreply.github.com>
2023-12-04 19:12:13 +0100

    Add files via upload

---
+++ Domasno 09/index/script.js
@@ -0,0 +1,25 @@
+// exercise 1
+let feet = 20;
+let conversionRate = 0.3408;
+let midresult = feet * conversionRate;
+console.log("20feets to metars:", midresult);
+// exercise 2
+let a = 5;
+let b = 10;
+let area = a * b;
+console.log("area is: ", area);
+// exercise 3
+let r = 2;
+let pi = 3.14;
+let circleArea = r * r * pi;
+console.log("plostina na krugot: ",circleArea);
+// Homework
+let quantity=30;
+let price=119.95;
+let tax=0.05;
+let totalWithoutTax=quantity*price;
+let total=totalWithoutTax+totalWithoutTax*tax;
+console.log("total with tax: ",total);
+
+
+
